a089db41ec84e129a61db71b80920c3d5f89126f
janghyunjun <buzz03312@naver.com>
2025-09-02 18:57:50 +0900

    [init] 초기 프로젝트 셋팅

---
+++ src/main/java/konkuk/chacall/ChacallApplication.java
@@ -0,0 +1,13 @@
+package konkuk.chacall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChacallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChacallApplication.class, args);
+	}
+
+}
