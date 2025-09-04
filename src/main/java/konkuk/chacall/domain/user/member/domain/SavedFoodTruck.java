4acdb1db6ca39241b4b3d6a5bc369b969f3874b2
janghyunjun <buzz03312@naver.com>
2025-09-04 15:56:28 +0900

    [init] SavedFoodTruck 도메인

---
+++ src/main/java/konkuk/chacall/domain/user/member/domain/SavedFoodTruck.java
@@ -0,0 +1,28 @@
+package konkuk.chacall.domain.user.member.domain;
+
+import jakarta.persistence.*;
+import konkuk.chacall.domain.foodtruck.domain.FoodTruck;
+import konkuk.chacall.domain.user.User;
+import konkuk.chacall.global.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "saved_food_trucks")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SavedFoodTruck extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "saved_food_truck_id", nullable = false)
+    private Long savedFoodTruckId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_truck_id", nullable = false)
+    private FoodTruck foodTruck;
+
+}
