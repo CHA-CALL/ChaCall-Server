27380bf4cb64b91af384712f94ddd196f1269fb4
janghyunjun <buzz03312@naver.com>
2025-09-04 15:56:01 +0900

    [init] Rating 도메인

---
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/Rating.java
@@ -0,0 +1,33 @@
+package konkuk.chacall.domain.foodtruck.domain;
+
+import jakarta.persistence.*;
+import konkuk.chacall.domain.user.User;
+import konkuk.chacall.global.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ratings")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Rating extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rating_id", nullable = false)
+    private Long ratingId;
+
+    private Double rating;
+
+    @Column(nullable = false)
+    private boolean isRated;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_truck_id", nullable = false)
+    private FoodTruck foodTruck;
+}
