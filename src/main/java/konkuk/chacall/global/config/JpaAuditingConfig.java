657b266863fce4d1b3d36ddf4d3b4b3c93377a46
janghyunjun <buzz03312@naver.com>
2025-09-04 15:53:04 +0900

    [init] JpaAuditingConfig

---
+++ src/main/java/konkuk/chacall/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package konkuk.chacall.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
