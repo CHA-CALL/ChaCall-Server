a1de3d9d341ec5233943a2a1ec3d9b2e6f4f3f18
janghyunjun <buzz03312@naver.com>
2025-09-04 15:55:38 +0900

    [init] 일반 유저 도메인

---
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/Menu.java
@@ -0,0 +1,33 @@
+package konkuk.chacall.domain.foodtruck.domain;
+
+import jakarta.persistence.*;
+import konkuk.chacall.global.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "menus")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Menu extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_id", nullable = false)
+    private Long menuId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    private String description;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_truck_id", nullable = false)
+    private FoodTruck foodTruck;
+}
---
+++ src/main/java/konkuk/chacall/domain/user/member/presentation/MemberController.java
@@ -0,0 +1,4 @@
+package konkuk.chacall.domain.user.member.presentation;
+
+public class MemberController {
+}
