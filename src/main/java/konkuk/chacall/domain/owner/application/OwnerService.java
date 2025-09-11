7277ad618ca7b27cc7a44cb23a78bd4717bd186b
ksg <ksg001227@naver.com>
2025-09-11 17:49:47 +0900

    [feat] 계좌 등록 API 구현 (#3)

--- src/main/java/konkuk/chacall/domain/owner/application/OwnerService.java
+++ src/main/java/konkuk/chacall/domain/owner/application/OwnerService.java
@@ -1,4 +1,17 @@
 package konkuk.chacall.domain.owner.application;
 
+import konkuk.chacall.domain.owner.application.bankAccount.BankAccountService;
+import konkuk.chacall.domain.owner.presentation.dto.RegisterBankAccountRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class OwnerService {
+
+    private final BankAccountService bankAccountService;
+
+    public void registerBankAccount(RegisterBankAccountRequest request, Long ownerId) {
+        bankAccountService.registerBankAccount(request, ownerId);
+    }
 }
--- src/main/java/konkuk/chacall/domain/owner/presentation/OwnerController.java
+++ src/main/java/konkuk/chacall/domain/owner/presentation/OwnerController.java
@@ -1,4 +1,30 @@
 package konkuk.chacall.domain.owner.presentation;
 
+import jakarta.validation.Valid;
+import konkuk.chacall.domain.owner.application.OwnerService;
+import konkuk.chacall.domain.owner.presentation.dto.RegisterBankAccountRequest;
+import konkuk.chacall.global.common.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/owners")
+@Slf4j
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @PostMapping("/me/bank-accounts")
+    public BaseResponse<Void> registerBankAccount(
+            @Valid RegisterBankAccountRequest registerBankAccountRequest
+    ) {
+        // todo 추후에 토큰 추가될 시 id 값은 토큰에서 추출하여 전달
+        ownerService.registerBankAccount(registerBankAccountRequest, 1L);
+
+        return BaseResponse.ok(null);
+    }
 }
