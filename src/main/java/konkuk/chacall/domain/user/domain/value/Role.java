60becacc21b915b3289156d3f101a4a3c2ca1b5c
janghyunjun <buzz03312@naver.com>
2025-09-14 01:33:50 +0900

    [chore] user 관련 값객체 domain.value 하위로 이동 (#4)

--- src/main/java/konkuk/chacall/domain/user/domain/value/Role.java
+++ src/main/java/konkuk/chacall/domain/user/domain/value/Role.java
@@ -1,10 +1,13 @@
-package konkuk.chacall.domain.user;
+package konkuk.chacall.domain.user.domain.value;
 
 import lombok.Getter;
 
 @Getter
 public enum Role {
-    MEMBER("일반 회원"), OWNER("사장님");
+    MEMBER("일반 회원"),
+    OWNER("사장님"),
+    NON_SELECTED("선택 안함")
+    ;
 
     private final String value;
 
