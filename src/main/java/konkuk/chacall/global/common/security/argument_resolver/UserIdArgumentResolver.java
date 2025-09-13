5c7a1bbf5168c7adec6398cc6a063b19900fdc30
janghyunjun <buzz03312@naver.com>
2025-09-14 01:39:44 +0900

    [refactor] userId ArgumentResolver 정의 (#4)

---
+++ src/main/java/konkuk/chacall/global/common/security/argument_resolver/UserIdArgumentResolver.java
@@ -0,0 +1,39 @@
+package konkuk.chacall.global.common.security.argument_resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import konkuk.chacall.global.common.exception.AuthException;
+import konkuk.chacall.global.common.security.annotation.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static konkuk.chacall.global.common.exception.code.ErrorCode.AUTH_TOKEN_NOT_FOUND;
+
+
+@Component
+@RequiredArgsConstructor
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(UserId.class)
+                && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Long resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+
+        Object userId = ((HttpServletRequest) webRequest.getNativeRequest()).getAttribute("userId");
+        if (userId == null) {
+            throw new AuthException(AUTH_TOKEN_NOT_FOUND);
+        }
+        return (Long) userId;
+    }
+}
