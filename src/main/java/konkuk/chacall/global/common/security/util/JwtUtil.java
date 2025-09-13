e4b34cc29b5001f4af993ab9916bb3d683b12fac
janghyunjun <buzz03312@naver.com>
2025-09-14 01:36:58 +0900

    [feat] Jwt 유틸 객체 정의 (#4)

---
+++ src/main/java/konkuk/chacall/global/common/security/util/JwtUtil.java
@@ -0,0 +1,71 @@
+package konkuk.chacall.global.common.security.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
+import konkuk.chacall.global.common.security.oauth2.LoginUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import static konkuk.chacall.global.common.security.constant.AuthParameters.JWT_ACCESS_TOKEN_KEY;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    //todo 확정 후 환경변수로 변경
+    private final long tokenExpiredMs = 2592000000L; // 30일
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String createAccessToken(Long userId) {
+        return Jwts.builder()
+                .claim(JWT_ACCESS_TOKEN_KEY.getValue(), userId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + tokenExpiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (SignatureException e) {
+            log.info("Invalid JWT Signature", e);
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    private Long getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get(JWT_ACCESS_TOKEN_KEY.getValue(), Long.class);
+    }
+
+    public LoginUser getLoginUser(String token) {
+        Long userId = getUserId(token);
+
+        return LoginUser.createLoginUser(userId);
+    }
+
+}
