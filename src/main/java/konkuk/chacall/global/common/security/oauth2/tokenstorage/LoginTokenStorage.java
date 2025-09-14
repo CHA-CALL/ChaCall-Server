c43b6aacc4d2207c73cf675a5a3115c58ef76be6
janghyunjun <buzz03312@naver.com>
2025-09-14 13:08:03 +0900

    [fest] 로그인 키 스토리지를 메모리 기반에서 Redis 기반으로 수정 (#4)

--- src/main/java/konkuk/chacall/global/common/security/oauth2/tokenstorage/LoginTokenStorage.java
+++ src/main/java/konkuk/chacall/global/common/security/oauth2/tokenstorage/LoginTokenStorage.java
@@ -2,48 +2,29 @@ package konkuk.chacall.global.common.security.oauth2.tokenstorage;
 
 import konkuk.chacall.domain.user.domain.value.Role;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.NoArgsConstructor;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.ConcurrentHashMap;
 
-@Component
-@RequiredArgsConstructor
-public class LoginTokenStorage {
+public interface LoginTokenStorage {
+
+    void put(String key, Role role, String token, Duration ttl);
+
+    /**
+     * 저장된 토큰을 1회용으로 소비 후 삭제한다.
+     * 존재하지 않으면 null 반환.
+     */
+    Entry consume(String key);
 
     @Getter
-    public static final class Entry {
-        private final String token;
-        private final Role role;
-        private final long expireAtEpochMillis;
+    @NoArgsConstructor
+    final class Entry {
+        private Role role;
+        private String token;
 
-        private Entry(String token, Role role,  long expireAtEpochMillis) {
-            this.token = token;
+        public Entry(Role role, String token) {
             this.role = role;
-            this.expireAtEpochMillis = expireAtEpochMillis;
+            this.token = token;
         }
-    }
-
-    private final ConcurrentHashMap<String, Entry> store = new ConcurrentHashMap<>();
-
-    /**
-     * 토큰을 메모리에 저장 (TTL 적용)
-     */
-    public void put(String key, Role role, String token, Duration ttl) {
-        long expiredAt = Instant.now().plus(ttl).toEpochMilli();
-        store.put(key, new Entry(token, role, expiredAt));
-    }
-
-    /**
-     * 토큰을 일회성으로 조회 후 제거 (만료 시 null 반환)
-     */
-    public Entry consume(String key) {
-        Entry entry = store.remove(key);
-
-        if (entry == null) return null;
-        if (entry.expireAtEpochMillis < Instant.now().toEpochMilli()) return null;
-        return entry;
     }
 }
