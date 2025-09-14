9756459acd6189878eb2ca172d3730ae34e00972
janghyunjun <buzz03312@naver.com>
2025-09-14 12:28:53 +0900

    [test] 소셜 로그인 테스트를 위한 정적 서빙 파일 추가 (테스트 이후 삭제 예정) (#4)

--- src/main/java/konkuk/chacall/global/config/SecurityConfig.java
+++ src/main/java/konkuk/chacall/global/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             "/login/oauth2/code/**", "/actuator/health",
             "/auth/users", "/auth/token",
 
-            "/api/test/**",  // for test
+            "/index.html", "/static/**",   // for test
     };
 
     @Bean
