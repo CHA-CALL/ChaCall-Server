070f2106c9b1fc436448daac70d76199e0a884cb
ksg <ksg001227@naver.com>
2025-09-15 17:18:33 +0900

    [feat] 예약 내역 조회용 리포지토리 메서드 구현 (#12)

--- src/main/java/konkuk/chacall/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/konkuk/chacall/domain/reservation/domain/repository/ReservationRepository.java
@@ -1,7 +1,19 @@
 package konkuk.chacall.domain.reservation.domain.repository;
 
 import konkuk.chacall.domain.reservation.domain.model.Reservation;
+import konkuk.chacall.domain.reservation.domain.value.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH r.member m " +                  // 예약자 정보 함께 조회
+            "JOIN FETCH r.foodTruck ft " +              // 푸드트럭 정보 함께 조회
+            "JOIN FETCH ft.owner o " +                  // 푸드트럭의 사장님 정보 함께 조회
+            "WHERE o.userId = :ownerId AND r.reservationStatus = :status ")
+    List<Reservation> findOwnerReservationsByStatus(@Param("ownerId") Long ownerId, @Param("status") ReservationStatus status);
 }
