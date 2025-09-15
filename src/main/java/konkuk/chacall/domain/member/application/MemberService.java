e96b7b6df05afb12946fda30608c8719c92f7a6e
janghyunjun <buzz03312@naver.com>
2025-09-15 18:07:50 +0900

    [feat] 푸드트럭 저장상태 변경 api 핸들러 정의 (#14)

--- src/main/java/konkuk/chacall/domain/member/application/MemberService.java
+++ src/main/java/konkuk/chacall/domain/member/application/MemberService.java
@@ -1,4 +1,18 @@
 package konkuk.chacall.domain.member.application;
 
+import konkuk.chacall.domain.member.application.foodtruck.SavedFoodTruckService;
+import konkuk.chacall.domain.member.presentation.dto.request.UpdateFoodTruckSaveStatusRequest;
+import konkuk.chacall.domain.member.presentation.dto.response.SavedFoodTruckResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final SavedFoodTruckService savedFoodTruckService;
+
+    public SavedFoodTruckResponse updateFoodTruckSaveStatus(UpdateFoodTruckSaveStatusRequest request, Long foodTruckId, Long userId) {
+        return savedFoodTruckService.updateFoodTruckSaveStatus(request, foodTruckId, userId);
+    }
 }
--- src/main/java/konkuk/chacall/domain/member/presentation/MemberController.java
+++ src/main/java/konkuk/chacall/domain/member/presentation/MemberController.java
@@ -1,4 +1,30 @@
 package konkuk.chacall.domain.member.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import konkuk.chacall.domain.member.application.MemberService;
+import konkuk.chacall.domain.member.presentation.dto.request.UpdateFoodTruckSaveStatusRequest;
+import konkuk.chacall.domain.member.presentation.dto.response.SavedFoodTruckResponse;
+import konkuk.chacall.global.common.dto.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Member API", description = "일반 유저 관련 API")
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Operation(summary = "푸드트럭 저장상태 변경", description = "푸드트럭 저장상태를 변경합니다. (저장/저장취소)" )
+    @PatchMapping("/me/food-trucks/{foodTruckId}")
+    public BaseResponse<SavedFoodTruckResponse> updateFoodTruckSaveStatus(
+            @Parameter(description = "푸드트럭 식별자", example = "1") final @PathVariable Long foodTruckId,
+            final @RequestBody @Valid UpdateFoodTruckSaveStatusRequest request
+    ) {
+        return BaseResponse.ok(memberService.updateFoodTruckSaveStatus(request, foodTruckId, 1L));
+    }
 }
