6f382046fce0047b7aed3e16138b26fcff55a38d
janghyunjun <buzz03312@naver.com>
2025-09-15 21:27:34 +0900

    [refactor] member로 변수 네이밍 변경 (#11)

--- src/main/java/konkuk/chacall/domain/member/application/MemberService.java
+++ src/main/java/konkuk/chacall/domain/member/application/MemberService.java
@@ -1,6 +1,8 @@
 package konkuk.chacall.domain.member.application;
 
+import jakarta.validation.Valid;
 import konkuk.chacall.domain.member.application.foodtruck.SavedFoodTruckService;
+import konkuk.chacall.domain.member.presentation.dto.request.RegisterRatingRequest;
 import konkuk.chacall.domain.member.presentation.dto.request.UpdateFoodTruckSaveStatusRequest;
 import konkuk.chacall.domain.member.presentation.dto.response.SavedFoodTruckStatusResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,11 @@ public class MemberService {
 
     private final SavedFoodTruckService savedFoodTruckService;
 
-    public SavedFoodTruckStatusResponse updateFoodTruckSaveStatus(UpdateFoodTruckSaveStatusRequest request, Long foodTruckId, Long userId) {
-        return savedFoodTruckService.updateFoodTruckSaveStatus(request, foodTruckId, userId);
+    public SavedFoodTruckStatusResponse updateFoodTruckSaveStatus(UpdateFoodTruckSaveStatusRequest request, Long foodTruckId, Long memberId) {
+        return savedFoodTruckService.updateFoodTruckSaveStatus(request, foodTruckId, memberId);
+    }
+
+    public Void registerRatings(RegisterRatingRequest request, Long memberId) {
+
     }
 }
