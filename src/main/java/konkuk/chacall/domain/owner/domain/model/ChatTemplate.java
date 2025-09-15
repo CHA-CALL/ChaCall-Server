fbb192562997009b49eebfa6bd4627b35589edfb
ksg <ksg001227@naver.com>
2025-09-15 15:07:54 +0900

    [feat] ChatTemplate 생성 정적 메서드 및 수정 메서드 구현 (#7)

--- src/main/java/konkuk/chacall/domain/owner/domain/model/ChatTemplate.java
+++ src/main/java/konkuk/chacall/domain/owner/domain/model/ChatTemplate.java
@@ -3,12 +3,14 @@ package konkuk.chacall.domain.owner.domain.model;
 import jakarta.persistence.*;
 import konkuk.chacall.domain.user.domain.model.User;
 import konkuk.chacall.global.common.domain.BaseEntity;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Getter
 @Entity
 @Table(name = "chat_templates")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class ChatTemplate extends BaseEntity {
 
     @Id
@@ -21,4 +23,15 @@ public class ChatTemplate extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User owner;
+
+    public static ChatTemplate of(String content, User owner) {
+        return ChatTemplate.builder()
+                .content(content)
+                .owner(owner)
+                .build();
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
---
+++ src/main/java/konkuk/chacall/domain/owner/presentation/dto/request/RegisterChatTemplateRequest.java
@@ -0,0 +1,2 @@
+package konkuk.chacall.domain.owner.presentation.dto.request;public record RegisterChatTemplateRequest() {
+}
