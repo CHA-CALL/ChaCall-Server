c213ef130184aed21760383510bff70703ce9af6
ksg <ksg001227@naver.com>
2025-09-15 16:32:17 +0900

    [chore] 자주 쓰는 채팅 최대글자수 설정 (#7)

--- src/main/java/konkuk/chacall/domain/owner/presentation/dto/request/RegisterChatTemplateRequest.java
+++ src/main/java/konkuk/chacall/domain/owner/presentation/dto/request/RegisterChatTemplateRequest.java
@@ -2,9 +2,11 @@ package konkuk.chacall.domain.owner.presentation.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterChatTemplateRequest(
         @Schema(description = "자주 쓰는 채팅 내용", example = "안녕하세요. 차콜 푸드트럭입니다!")
+        @Size(max = 500, message = "최대 500자까지 입력 가능합니다.")
         @NotBlank(message = "자주 쓰는 채팅을 입력해야합니다.")
         String content
 ) {
