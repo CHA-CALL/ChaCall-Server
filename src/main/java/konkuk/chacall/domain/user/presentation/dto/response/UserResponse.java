59ec95c4a42d4b2cd5367fcc1934b89ddfc3eb89
janghyunjun <buzz03312@naver.com>
2025-09-17 01:22:34 +0900

    [docs] 마이페이지 조회시 dto 명세 추가 (#14)

--- src/main/java/konkuk/chacall/domain/user/presentation/dto/response/UserResponse.java
+++ src/main/java/konkuk/chacall/domain/user/presentation/dto/response/UserResponse.java
@@ -1,12 +1,18 @@
 package konkuk.chacall.domain.user.presentation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import konkuk.chacall.domain.user.domain.model.User;
 
 public record UserResponse(
+        @Schema(description = "프로필 이미지 URL", example = "http://image.png", nullable = true)
         String profileImageUrl,
+        @Schema(description = "이름", example = "홍길동")
         String name,
+        @Schema(description = "이메일", example = "email@email.com")
         String email,
+        @Schema(description = "성별", example = "남성")
         String gender,
+        @Schema(description = "약관 동의 여부", example = "true")
         boolean termAgreed
 ) {
     public static UserResponse from(User user) {
