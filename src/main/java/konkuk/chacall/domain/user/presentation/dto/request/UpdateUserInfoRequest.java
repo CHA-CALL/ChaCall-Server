6f9150aef997bf3c4fdd4ae5c2aa71fb01fe67b0
janghyunjun <buzz03312@naver.com>
2025-09-16 15:15:26 +0900

    [refactor] 사용자 정보 수정 요청 dto의 이메일 형식 검증 추가 (#11)

--- src/main/java/konkuk/chacall/domain/user/presentation/dto/request/UpdateUserInfoRequest.java
+++ src/main/java/konkuk/chacall/domain/user/presentation/dto/request/UpdateUserInfoRequest.java
@@ -1,6 +1,7 @@
 package konkuk.chacall.domain.user.presentation.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -17,6 +18,7 @@ public record UpdateUserInfoRequest(
 
         @Schema(description = "사용자 이메일", example = "chacall@kokuk.ac.kr")
         @NotBlank(message = "이메일은 비어 있을 수 없습니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
         @Schema(description = "사용자 성별", example = "남성")
