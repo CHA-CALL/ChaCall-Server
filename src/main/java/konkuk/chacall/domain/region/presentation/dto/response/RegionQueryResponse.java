c8620990cf23d9d39b74945c3ce9bf7afb95d620
ksg <ksg001227@naver.com>
2025-09-18 23:49:43 +0900

    [feat] 지역 조회 응답 DTO 구현 (#22)

--- src/main/java/konkuk/chacall/domain/region/presentation/dto/response/RegionQueryResponse.java
+++ src/main/java/konkuk/chacall/domain/region/presentation/dto/response/RegionQueryResponse.java
@@ -1,2 +1,18 @@
-package konkuk.chacall.domain.region.presentation.dto.response;public record RegionQueryResponse() {
+package konkuk.chacall.domain.region.presentation.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import konkuk.chacall.domain.region.domain.model.Region;
+
+public record RegionQueryResponse(
+        @Schema(description = "지역 이름", example = "서울")
+        String name,
+
+        @Schema(description = "지역 행정동 코드", example = "11")
+        Long code
+) {
+        public static RegionQueryResponse of(Region region) {
+                return new RegionQueryResponse(
+                        region.getName(),
+                        region.getRegionCode());
+        }
 }
