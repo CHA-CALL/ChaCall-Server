4215554b4f758d4e1b156f243e1b360a6423a5fd
ksg <ksg001227@naver.com>
2025-09-19 00:24:06 +0900

    [feat] 지역 조회 API 구현 (#22)

--- src/main/java/konkuk/chacall/domain/region/presentation/RegionController.java
+++ src/main/java/konkuk/chacall/domain/region/presentation/RegionController.java
@@ -1,4 +1,42 @@
 package konkuk.chacall.domain.region.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import konkuk.chacall.domain.owner.presentation.dto.request.RegisterBankAccountRequest;
+import konkuk.chacall.domain.region.application.RegionService;
+import konkuk.chacall.domain.region.presentation.dto.request.RegionQueryRequest;
+import konkuk.chacall.domain.region.presentation.dto.response.RegionQueryResponse;
+import konkuk.chacall.global.common.annotation.ExceptionDescription;
+import konkuk.chacall.global.common.annotation.UserId;
+import konkuk.chacall.global.common.dto.BaseResponse;
+import konkuk.chacall.global.common.swagger.SwaggerResponseDescription;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Region API", description = "지역 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/regions")
+@Slf4j
 public class RegionController {
+
+    private final RegionService regionService;
+
+    @Operation(
+            summary = "지역 조회",
+            description = "depth, parentCode 를 기반으로 지역을 조회합니다."
+    )
+    @ExceptionDescription(SwaggerResponseDescription.GET_REGIONS)
+    @GetMapping
+    public BaseResponse<List<RegionQueryResponse>> getRegions(
+            @Valid @ParameterObject final RegionQueryRequest request
+    ) {
+        return BaseResponse.ok(regionService.getRegions(request));
+    }
 }
