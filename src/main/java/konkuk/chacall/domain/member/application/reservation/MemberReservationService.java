2a41f31a5740c4d51e0224714773df14c9b27ac5
janghyunjun <buzz03312@naver.com>
2025-09-24 02:32:34 +0900

    [refactor] 일반유저 예약 내역 조회시 탭에 따른 조회 로직 수정 (#28)

--- src/main/java/konkuk/chacall/domain/member/application/reservation/MemberReservationService.java
+++ src/main/java/konkuk/chacall/domain/member/application/reservation/MemberReservationService.java
@@ -5,16 +5,15 @@ import konkuk.chacall.domain.member.presentation.dto.response.MemberReservationD
 import konkuk.chacall.domain.member.presentation.dto.response.MemberReservationHistoryResponse;
 import konkuk.chacall.domain.reservation.domain.model.Reservation;
 import konkuk.chacall.domain.reservation.domain.repository.ReservationRepository;
+import konkuk.chacall.domain.reservation.domain.value.ReservationViewType;
 import konkuk.chacall.domain.user.domain.model.User;
 import konkuk.chacall.global.common.dto.CursorPagingResponse;
-import konkuk.chacall.global.common.exception.BusinessException;
 import konkuk.chacall.global.common.exception.EntityNotFoundException;
 import konkuk.chacall.global.common.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,9 +23,9 @@ public class MemberReservationService {
 
     private final ReservationRepository reservationRepository;
 
-    public CursorPagingResponse<MemberReservationHistoryResponse> getMemberReservations(User member, GetReservationHistoryRequest request, Long lastCursor, int pageSize) {
+    public CursorPagingResponse<MemberReservationHistoryResponse> getMemberReservations(User member, ReservationViewType viewType, Long lastCursor, int pageSize) {
         Slice<Reservation> memberReservationSlice = reservationRepository
-                .findMemberReservationsByStatusWithCursor(member.getUserId(), request.status(), lastCursor, PageRequest.of(0, pageSize));
+                .findMemberReservationsByStatusWithCursor(member.getUserId(), viewType.getStatuses(), lastCursor, PageRequest.of(0, pageSize));
         List<Reservation> memberReservationList = memberReservationSlice.getContent();
 
         List<MemberReservationHistoryResponse> responses = memberReservationList.stream()
--- src/main/java/konkuk/chacall/domain/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/konkuk/chacall/domain/reservation/domain/repository/ReservationRepository.java
@@ -36,12 +36,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @EntityGraph(attributePaths = {"foodTruck"})
     @Query("SELECT r FROM Reservation r " +
             "WHERE r.member.userId = :memberId " +
-            "AND r.reservationStatus = :status " +
+            "AND r.reservationStatus IN :statuses " +
             "AND r.reservationId < :lastCursor " +
             "ORDER BY r.reservationId DESC")
     Slice<Reservation> findMemberReservationsByStatusWithCursor(
             @Param("memberId") Long memberId,
-            @Param("status") ReservationStatus status,
+            @Param("statuses") Set<ReservationStatus> statuses,
             @Param("lastCursor") Long lastCursor,
             Pageable pageable);
 
