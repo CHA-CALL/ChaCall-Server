bf16368f01dc5d910dda34e7a4b04b62a608e033
ksg <ksg001227@naver.com>
2025-09-25 17:20:58 +0900

    [feat] 메뉴 노출 여부 컬럼 추가 (#32)

--- src/main/java/konkuk/chacall/domain/foodtruck/domain/model/Menu.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/model/Menu.java
@@ -1,6 +1,7 @@
 package konkuk.chacall.domain.foodtruck.domain.model;
 
 import jakarta.persistence.*;
+import konkuk.chacall.domain.foodtruck.domain.value.MenuStatus;
 import konkuk.chacall.global.common.domain.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,15 +18,21 @@ public class Menu extends BaseEntity {
     @Column(name = "menu_id", nullable = false)
     private Long menuId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 18)
     private String name;
 
     @Column(nullable = false)
     private Integer price;
 
+    @Column(length = 50)
     private String description;
 
+    @Column(length = 2083)
     private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private MenuStatus status = MenuStatus.ON;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "food_truck_id", nullable = false)
