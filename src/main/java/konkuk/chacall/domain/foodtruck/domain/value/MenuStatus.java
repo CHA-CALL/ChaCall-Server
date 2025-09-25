625c4932962474e0c9ce5bfb89565ce2bee7470c
ksg <ksg001227@naver.com>
2025-09-25 20:12:53 +0900

    [feat] 메뉴 상태(표시 여부) 변경용 DTO 구현 (#32)

--- src/main/java/konkuk/chacall/domain/foodtruck/domain/value/MenuStatus.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/value/MenuStatus.java
@@ -1,14 +1,12 @@
 package konkuk.chacall.domain.foodtruck.domain.value;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
 public enum MenuStatus {
-    ON("노출"), // 메뉴 표시 여부
-    OFF("비노출");
+    ON("표시"),
+    OFF("미표시");
 
-    @JsonValue
     private final String value;
 
     MenuStatus(String value) {
