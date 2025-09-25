84e84763ba648c7082eefd6f58bea1cdb3d4342b
ksg <ksg001227@naver.com>
2025-09-25 17:29:49 +0900

    [feat] 메뉴 목록 조회용 리포지토리 메서드 구현 (#32)
    - 오름차순 / 내림차순 모두 구현

--- src/main/java/konkuk/chacall/domain/foodtruck/domain/repository/MenuRepository.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/repository/MenuRepository.java
@@ -1,6 +1,8 @@
 package konkuk.chacall.domain.foodtruck.domain.repository;
 
 import konkuk.chacall.domain.foodtruck.domain.model.Menu;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,26 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
     @Modifying
     @Query("DELETE FROM Menu m WHERE m.foodTruck.foodTruckId = :foodTruckId")
     void deleteAllByFoodTruckId(@Param("foodTruckId") Long foodTruckId);
+
+    @Query("""
+        select m
+        from Menu m
+        where m.foodTruck.foodTruckId = :foodTruckId
+          and m.menuId < :lastCursor
+        order by m.menuId desc
+        """)
+    Slice<Menu> findMenusDesc(@Param("foodTruckId") Long foodTruckId,
+                              @Param("lastCursor") Long lastCursor,
+                              Pageable pageable);
+
+    @Query("""
+        select m
+        from Menu m
+        where m.foodTruck.foodTruckId = :foodTruckId
+          and m.menuId > :lastCursor
+        order by m.menuId asc
+        """)
+    Slice<Menu> findMenusAsc(@Param("foodTruckId") Long foodTruckId,
+                              @Param("lastCursor") Long lastCursor,
+                              Pageable pageable);
 }
