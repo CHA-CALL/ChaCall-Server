e0515e36c3aadf6506d605c0184b3ce84f230b3f
ksg <ksg001227@naver.com>
2025-09-25 20:10:31 +0900

    [feat] 푸드트럭 이름 중복 체크 API 구현 (#32)

--- src/main/java/konkuk/chacall/domain/foodtruck/application/FoodTruckService.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/application/FoodTruckService.java
@@ -1,7 +1,10 @@
 package konkuk.chacall.domain.foodtruck.application;
 
+import konkuk.chacall.domain.foodtruck.application.duplicatecheck.FoodTruckNameDuplicateCheckService;
 import konkuk.chacall.domain.foodtruck.application.search.FoodTruckSearchService;
+import konkuk.chacall.domain.foodtruck.presentation.dto.request.FoodTruckNameDuplicateCheckRequest;
 import konkuk.chacall.domain.foodtruck.presentation.dto.request.FoodTruckSearchRequest;
+import konkuk.chacall.domain.foodtruck.presentation.dto.response.FoodTruckNameDuplicateCheckResponse;
 import konkuk.chacall.domain.foodtruck.presentation.dto.response.FoodTruckResponse;
 import konkuk.chacall.global.common.dto.CursorPagingResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class FoodTruckService {
 
     private final FoodTruckSearchService foodTruckSearchService;
+    private final FoodTruckNameDuplicateCheckService foodTruckNameDuplicateCheckService;
 
     public CursorPagingResponse<FoodTruckResponse> getFoodTrucks(FoodTruckSearchRequest request) {
         return foodTruckSearchService.getFoodTrucks(request);
+    }
+
+    public FoodTruckNameDuplicateCheckResponse isNameDuplicated(FoodTruckNameDuplicateCheckRequest request) {
+        return FoodTruckNameDuplicateCheckResponse.of(
+                foodTruckNameDuplicateCheckService.isNameDuplicated(request.name()));
     }
 }
