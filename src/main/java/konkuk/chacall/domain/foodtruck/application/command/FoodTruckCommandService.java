7c8a85fd568265b5d392a4152d8e08d890cbce58
ksg <ksg001227@naver.com>
2025-09-29 14:12:33 +0900

    [feat] 푸드트럭 이름 검증시 소문자/대문자 구분 (#32)

--- src/main/java/konkuk/chacall/domain/foodtruck/application/command/FoodTruckCommandService.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/application/command/FoodTruckCommandService.java
@@ -32,6 +32,6 @@ public class FoodTruckCommandService {
     }
 
     public boolean isNameDuplicated(String name) {
-        return foodTruckRepository.existsByNameIgnoreCase(name);
+        return foodTruckRepository.existsByName(name);
     }
 }
