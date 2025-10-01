b22b7b2c9053bc74bed1bc768b2a3ae5858d6c9d
janghyunjun <buzz03312@naver.com>
2025-10-01 17:19:00 +0900

    [feat] 푸드트럭 및 메뉴 이미지 key 발급 로직 (#35)

--- src/main/java/konkuk/chacall/global/common/storage/util/KeyUtils.java
+++ src/main/java/konkuk/chacall/global/common/storage/util/KeyUtils.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class KeyUtils {
@@ -18,5 +19,23 @@ public final class KeyUtils {
                 RESERVATION_ESTIMATE_PREFIX,
                 timestamp,
                 reservationId);
+    }
+
+    public static String buildFoodTruckImageKey(Long userId) {
+        String timestamp = LocalDateTime.now().format(DATE_TIME_FORMATTER); // ex) 20250923-163045
+        String uuid = UUID.randomUUID().toString();
+        return String.format("foodtrucks/%d/%s/%s",
+                userId,
+                timestamp,
+                uuid); // foodtrucks/{userId}/{timestamp}/{uuid}
+    }
+
+    public static String buildMenuImageKey(Long foodTruckId) {
+        String timestamp = LocalDateTime.now().format(DATE_TIME_FORMATTER); // ex) 20250923-163045
+        String uuid = UUID.randomUUID().toString();
+        return String.format("menus/%d/%s/%s",
+                foodTruckId,
+                timestamp,
+                uuid); // menus/{foodTruckId}/{timestamp}/{uuid}
     }
 }
