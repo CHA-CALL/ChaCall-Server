73ff8d69a5786567334e4c70081e69eb2b4a200d
janghyunjun <buzz03312@naver.com>
2025-10-02 16:18:33 +0900

    [feat] 필요없는 주석 제거

--- src/main/java/konkuk/chacall/domain/test/TestController.java
+++ src/main/java/konkuk/chacall/domain/test/TestController.java
@@ -1,17 +1,7 @@
 package konkuk.chacall.domain.test;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import konkuk.chacall.global.common.dto.BaseResponse;
-import konkuk.chacall.global.common.exception.AuthException;
-import konkuk.chacall.global.common.exception.BusinessException;
-import konkuk.chacall.global.common.exception.DomainRuleException;
-import konkuk.chacall.global.common.exception.EntityNotFoundException;
-import konkuk.chacall.global.common.exception.code.ErrorCode;
 import konkuk.chacall.global.common.security.util.JwtUtil;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
