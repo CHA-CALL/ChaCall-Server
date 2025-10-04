ca1f551c23ec77459b8f46159e13b16ca687f85f
ksg <ksg001227@naver.com>
2025-10-05 00:26:59 +0900

    [chore] 푸드트럭 승인 상태 변경 스웨거 명세 수정 (#36)

--- src/main/java/konkuk/chacall/domain/user/presentation/dto/request/ApproveFoodTruckStatusRequest.java
+++ src/main/java/konkuk/chacall/domain/user/presentation/dto/request/ApproveFoodTruckStatusRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import konkuk.chacall.domain.foodtruck.domain.value.FoodTruckStatus;
 
 public record ApproveFoodTruckStatusRequest(
-        @Schema(description = "변경할 푸드트럭 승인 상태", example = "ON")
+        @Schema(description = "변경할 푸드트럭 승인 상태", example = "OFF")
         @NotNull(message = "승인 상태는 필수입니다.")
         FoodTruckStatus status
 ) {}
