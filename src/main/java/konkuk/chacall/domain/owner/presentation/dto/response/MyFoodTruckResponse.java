a9e961cb046992f70c7856f391dd8b624414bebb
ksg <ksg001227@naver.com>
2025-10-21 16:59:56 +0900

    [chore] 나의 푸드트럭 조회 응답 DTO 에 nullable 관련 명세 추가 (#48)

--- src/main/java/konkuk/chacall/domain/owner/presentation/dto/response/MyFoodTruckResponse.java
+++ src/main/java/konkuk/chacall/domain/owner/presentation/dto/response/MyFoodTruckResponse.java
@@ -9,13 +9,13 @@ import java.util.List;
 public record MyFoodTruckResponse(
         @Schema(description = "푸드트럭 식별자", example = "1")
         Long foodTruckId,
-        @Schema(description = "푸드트럭 이미지", example = "image.png")
+        @Schema(description = "푸드트럭 이미지", example = "image.png", nullable = true)
         String imageUrl,
         @Schema(description = "푸드트럭 이름", example = "차콜 푸드트럭")
         String name,
-        @Schema(description = "푸드트럭 설명", example = "저희 푸드트럭은 10년간 이어져온...")
+        @Schema(description = "푸드트럭 설명", example = "저희 푸드트럭은 10년간 이어져온...", nullable = true)
         String description,
-        @Schema(description = "운영 가능 시간대", example = "09:00 ~ 21:00")
+        @Schema(description = "운영 가능 시간대", example = "09:00 ~ 21:00", nullable = true)
         String activeTime,
         @Schema(description = "호출 가능 지역", example = "서울 전체, 경기도 수원시 영통구, 인천 계양구")
         String serviceArea,
