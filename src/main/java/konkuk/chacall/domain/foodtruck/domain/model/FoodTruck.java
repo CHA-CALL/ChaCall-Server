e8fa91442a70656bc593ab83e3281c96f03d8974
janghyunjun <buzz03312@naver.com>
2025-10-21 18:08:59 +0900

    [refactor] FoodTruck에서 FoodTruckInfo 객체 추출 (#49)

--- src/main/java/konkuk/chacall/domain/foodtruck/domain/model/FoodTruck.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/model/FoodTruck.java
@@ -26,39 +26,8 @@ public class FoodTruck extends BaseEntity {
     @Column(name = "food_truck_id", nullable = false)
     private Long foodTruckId;
 
-    @Column(nullable = false)
-    private String name;
-
-    @Column(length = 300)
-    private String description;
-
-    @Column(length = 15)
-    private String phoneNumber;
-
-    private String activeTime;
-
-    private Boolean timeDiscussRequired;
-
-    @Convert(converter = PhotoUrlListConverter.class)
-    private PhotoUrlList foodTruckPhotoList;
-
-    @Convert(converter = MenuCategoryListConverter.class)
-    private MenuCategoryList menuCategoryList;
-
-    @Enumerated(EnumType.STRING)
-    private AvailableQuantity availableQuantity;
-
-    @Enumerated(EnumType.STRING)
-    private NeedElectricity needElectricity;
-
-    @Enumerated(EnumType.STRING)
-    private PaymentMethod paymentMethod;
-
-    @Column(name = "operating_info", length = 800)
-    private String operatingInfo;
-
-    @Column(name = "option", length = 800)
-    private String option;
+    @Embedded
+    private FoodTruckInfo foodTruckInfo;
 
     @Column(name = "rejection_reason", length = 30)
     private String rejectionReason;
@@ -84,18 +53,7 @@ public class FoodTruck extends BaseEntity {
     public static FoodTruck createEmptyFoodTruck(User owner, String name) {
         return FoodTruck.builder()
                 .owner(owner)
-                .name(name)
-                .description(null)
-                .phoneNumber(null)
-                .activeTime(null)
-                .timeDiscussRequired(null)
-                .foodTruckPhotoList(null)
-                .menuCategoryList(null)
-                .availableQuantity(null)
-                .needElectricity(null)
-                .paymentMethod(null)
-                .operatingInfo(null)
-                .option(null)
+                .foodTruckInfo(FoodTruckInfo.createEmptyFoodTruckInfo(name))
                 .rejectionReason(null)
                 .foodTruckStatus(FoodTruckStatus.PENDING)
                 .foodTruckViewedStatus(FoodTruckViewedStatus.OFF)
@@ -110,6 +68,12 @@ public class FoodTruck extends BaseEntity {
     public void validateOwner(Long ownerId) {
         if (!isOwnedBy(ownerId)) {
             throw new DomainRuleException(ErrorCode.FOOD_TRUCK_NOT_OWNED);
+        }
+    }
+
+    public void validateApprovedStatus() {
+        if (this.foodTruckStatus != FoodTruckStatus.APPROVED) {
+            throw new DomainRuleException(ErrorCode.FOOD_TRUCK_NOT_APPROVED);
         }
     }
 
--- src/main/java/konkuk/chacall/domain/member/presentation/dto/response/MemberReservationDetailResponse.java
+++ src/main/java/konkuk/chacall/domain/member/presentation/dto/response/MemberReservationDetailResponse.java
@@ -2,6 +2,7 @@ package konkuk.chacall.domain.member.presentation.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import konkuk.chacall.domain.foodtruck.domain.model.FoodTruck;
+import konkuk.chacall.domain.foodtruck.domain.value.FoodTruckInfo;
 import konkuk.chacall.domain.reservation.domain.model.Reservation;
 import konkuk.chacall.domain.reservation.domain.value.ReservationStatus;
 
@@ -43,10 +44,11 @@ public record MemberReservationDetailResponse(
 
     public static MemberReservationDetailResponse of(Reservation reservation, FoodTruck foodTruck) {
         List<String> dateTimeList = reservation.getReservationInfo().getFormattedDateTimeInfos();
+        FoodTruckInfo foodTruckInfo = foodTruck.getFoodTruckInfo();
 
         return new MemberReservationDetailResponse(
-                foodTruck.getFoodTruckPhotoList().getMainPhotoUrl(),
-                foodTruck.getName(),
+                foodTruckInfo.getFoodTruckPhotoList().getMainPhotoUrl(),
+                foodTruckInfo.getName(),
                 reservation.getReservationInfo().getFullAddress(),
                 dateTimeList,
                 reservation.getReservationStatus() == ReservationStatus.CANCELLED ?  // 예약 취소 상태이면 null 반환
