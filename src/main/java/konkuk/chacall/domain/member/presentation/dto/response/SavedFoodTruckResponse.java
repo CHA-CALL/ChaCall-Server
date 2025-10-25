1470a2ddf5e7b8c8c85224135f66f6bf050e9a3f
janghyunjun <buzz03312@naver.com>
2025-10-26 00:48:48 +0900

    [refactor] 저장한 푸드트럭 목록 조회에서 메뉴 카테고리 리스트 반환 (#41)

--- src/main/java/konkuk/chacall/domain/member/presentation/dto/response/SavedFoodTruckResponse.java
+++ src/main/java/konkuk/chacall/domain/member/presentation/dto/response/SavedFoodTruckResponse.java
@@ -3,6 +3,8 @@ package konkuk.chacall.domain.member.presentation.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import konkuk.chacall.domain.foodtruck.domain.model.FoodTruck;
 
+import java.util.List;
+
 public record SavedFoodTruckResponse(
         @Schema(description = "푸드트럭 식별자", example = "1")
         Long foodTruckId,
@@ -12,6 +14,8 @@ public record SavedFoodTruckResponse(
         String photoUrl,
         @Schema(description = "푸드트럭 설명", example = "맛있는 푸드트럭입니다.")
         String description,
+        @Schema(description = "푸드트럭 음식 카테고리 (라벨 리스트)", example = "[\"한식\",\"분식\"]")
+        List<String> menuCategories,
         @Schema(description = "푸드트럭 평균 평점", example = "4.5")
         Double averageRating,
         @Schema(description = "푸드트럭 평점 수", example = "100")
@@ -23,6 +27,7 @@ public record SavedFoodTruckResponse(
                 foodTruck.getName(),
                 foodTruck.getFoodTruckPhotoList().getMainPhotoUrl(), // 대표 사진 (첫 번째 사진)
                 foodTruck.getDescription(),
+                foodTruck.getMenuCategoryList().getMenuCategoryLabelList(),
                 foodTruck.getRatingInfo().getAverageRating(),
                 foodTruck.getRatingInfo().getRatingCount()
         );
