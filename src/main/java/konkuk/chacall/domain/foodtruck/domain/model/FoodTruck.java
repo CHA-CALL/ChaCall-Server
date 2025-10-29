d705877f21f46db856f4d4be4915ade39cc2065e
janghyunjun <buzz03312@naver.com>
2025-10-29 21:24:03 +0900

    [feat] response dto 선언 (#53)

--- src/main/java/konkuk/chacall/domain/foodtruck/domain/model/FoodTruck.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/domain/model/FoodTruck.java
@@ -143,6 +143,13 @@ public class FoodTruck extends BaseEntity {
                 .collect(Collectors.joining(", "));
     }
 
+    // 푸드트럭의 운영 기간을 반환해주는 메서드
+    public List<String> getAvailableDates(List<AvailableDate> availableDateList) {
+        return availableDateList.stream()
+                .map(AvailableDate::formatDate)
+                .collect(Collectors.toList());
+    }
+
     public void changeViewedStatus(FoodTruckViewedStatus targetViewedStatus) {
         if(this.foodTruckViewedStatus == targetViewedStatus) {
             throw new DomainRuleException(ErrorCode.INVALID_FOOD_TRUCK_STATUS_TRANSITION);
@@ -154,5 +161,11 @@ public class FoodTruck extends BaseEntity {
         }
 
         this.foodTruckViewedStatus = targetViewedStatus;
+    }
+
+    public void validateViewableStatusForMember() {
+        if (this.foodTruckViewedStatus != FoodTruckViewedStatus.ON) {
+            throw new DomainRuleException(ErrorCode.FOOD_TRUCK_NOT_VIEWABLE);
+        }
     }
 }
---
+++ src/main/java/konkuk/chacall/domain/foodtruck/presentation/dto/response/FoodTruckDetailResponse.java
@@ -0,0 +1,69 @@
+package konkuk.chacall.domain.foodtruck.presentation.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import konkuk.chacall.domain.foodtruck.domain.model.AvailableDate;
+import konkuk.chacall.domain.foodtruck.domain.model.FoodTruck;
+import konkuk.chacall.domain.foodtruck.domain.model.FoodTruckServiceArea;
+import konkuk.chacall.domain.foodtruck.domain.value.FoodTruckInfo;
+
+import java.util.List;
+
+public record FoodTruckDetailResponse(
+        @Schema(description = "푸드트럭 식별자", example = "1")
+        Long foodTruckId,
+        @Schema(description = "푸드트럭 이름", example = "푸드트럭")
+        String name,
+        @Schema(description = "푸드트럭 설명", example = "맛있는 푸드트럭입니다.")
+        String description,
+        @Schema(description = "푸드트럭 전화번호", example = "010-1234-5678")
+        String phoneNumber,
+        @Schema(description = "푸드트럭 활동 시간", example = "09:00-20:00")
+        String activeTime,
+        @Schema(description = "시간 협의 필요 여부", example = "false")
+        Boolean timeDiscussRequired,
+        @Schema(description = "호출 가능 지역", example = "서울 광진구, 서울 강남구, 서울 영등포구")
+        String serviceAreas,
+        @Schema(description = "푸드트럭 메뉴 카테고리 (라벨 리스트)", example = "[\"한식\",\"분식\"]")
+        List<String> menuCategories,
+        @Schema(description = "푸드트럭 제공 가능 수량", example = "200인분 미만")
+        String availableQuantity,
+        @Schema(description = "전기 사용 필요 여부", example = "필요")
+        String needElectricity,
+        @Schema(description = "결제 방법", example = "무관")
+        String paymentMethod,
+        @Schema(description = "푸드트럭 제공 가능 날짜 리스트", example = "[\"2025-10-01 ~ 2025-10-10\",\"2025-11-01 ~ 2025-11-10\"]")
+        List<String> availableDates,
+        @Schema(description = "푸드트럭 사진 URL 리스트", example = "[\"http://image.png\",\"http://image2.png\",\"http://image3.png\"]")
+        List<String> photoUrl,
+        @Schema(description = "운영 정보", example = "운영정보")
+        String operatingInfo,
+        @Schema(description = "추가 옵션 정보", example = "안녕하세요")
+        String option,
+        @Schema(description = "푸드트럭 평균 평점", example = "4.5")
+        Double averageRating,
+        @Schema(description = "현재 사용자가 저장한 푸드트럭인지 여부", example = "true")
+        Boolean isSaved
+) {
+    public static FoodTruckDetailResponse from(FoodTruck foodTruck, List<FoodTruckServiceArea> serviceAreas, List<AvailableDate> availableDates, Boolean isSaved) {
+        FoodTruckInfo foodTruckInfo = foodTruck.getFoodTruckInfo();
+        return new FoodTruckDetailResponse(
+                foodTruck.getFoodTruckId(),
+                foodTruckInfo.getName(),
+                foodTruckInfo.getDescription(),
+                foodTruckInfo.getPhoneNumber(),
+                foodTruckInfo.getActiveTime(),
+                foodTruckInfo.getTimeDiscussRequired(),
+                foodTruck.getServiceAreas(serviceAreas),
+                foodTruckInfo.getMenuCategoryList().getMenuCategoryLabelList(),
+                foodTruckInfo.getAvailableQuantity().getValue(),
+                foodTruckInfo.getNeedElectricity().getValue(),
+                foodTruckInfo.getPaymentMethod().getValue(),
+                foodTruck.getAvailableDates(availableDates),
+                foodTruckInfo.getFoodTruckPhotoList().getUrls(),
+                foodTruckInfo.getOperatingInfo(),
+                foodTruckInfo.getOption(),
+                foodTruck.getRatingInfo().getAverageRating(),
+                isSaved
+        );
+    }
+}
