b0d259043393ec8209f96647cc31786c91f55dec
janghyunjun <buzz03312@naver.com>
2025-11-03 19:50:57 +0900

    [chore] Stomp를 위한 설정 파일 (#60)

---
+++ src/main/java/konkuk/chacall/global/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package konkuk.chacall.global.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // SockJS 사용 시
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app"); // 발신 prefix
+        registry.enableSimpleBroker("/topic"); // 수신 prefix
+    }
+}
