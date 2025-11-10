3276efc616383d45798f4494bfead082de474248
janghyunjun <buzz03312@naver.com>
2025-11-10 15:59:43 +0900

    [refactor] 채팅방에 예약 연관관계 주입 (#60)

--- src/main/java/konkuk/chacall/domain/chat/domain/ChatRoom.java
+++ src/main/java/konkuk/chacall/domain/chat/domain/ChatRoom.java
@@ -2,7 +2,10 @@ package konkuk.chacall.domain.chat.domain;
 
 import jakarta.persistence.*;
 import konkuk.chacall.domain.foodtruck.domain.model.FoodTruck;
+import konkuk.chacall.domain.reservation.domain.model.Reservation;
 import konkuk.chacall.domain.user.domain.model.User;
+import konkuk.chacall.global.common.exception.DomainRuleException;
+import konkuk.chacall.global.common.exception.code.ErrorCode;
 import lombok.*;
 
 @Entity
@@ -26,11 +29,22 @@ public class ChatRoom {
     @JoinColumn(name = "food_truck_id", nullable = false)
     private FoodTruck foodTruck;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
     public static ChatRoom createChatRoom(User member, FoodTruck foodTruck) {
         return ChatRoom.builder()
                 .member(member)
                 .foodTruck(foodTruck)
                 .build();
+    }
+
+    public void validateParticipant(User user) {
+        if(!this.member.getUserId().equals(user.getUserId()) &&
+           !this.foodTruck.getOwner().getUserId().equals(user.getUserId())) {
+            throw new DomainRuleException(ErrorCode.CHAT_ROOM_FORBIDDEN);
+        }
     }
 }
 
