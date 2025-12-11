d34d4345a3b35c262f9cfa882a2459bff8f1d6fc
janghyunjun <buzz03312@naver.com>
2025-12-11 17:17:36 +0900

    [refactor] 푸드트럭 상세조회 API에서 지역 응답 DTO로 반환

--- src/main/java/konkuk/chacall/domain/foodtruck/presentation/dto/response/FoodTruckDetailResponse.java
+++ src/main/java/konkuk/chacall/domain/foodtruck/presentation/dto/response/FoodTruckDetailResponse.java
@@ -6,6 +6,7 @@ import konkuk.chacall.domain.foodtruck.domain.model.FoodTruck;
 import konkuk.chacall.domain.foodtruck.domain.model.FoodTruckServiceArea;
 import konkuk.chacall.domain.foodtruck.domain.value.FoodTruckInfo;
 import konkuk.chacall.domain.region.domain.model.Region;
+import konkuk.chacall.domain.region.presentation.dto.response.RegionResponse;
 
 import java.util.List;
 
@@ -24,8 +25,8 @@ public record FoodTruckDetailResponse(
         Boolean timeDiscussRequired,
         @Schema(description = "호출 가능 지역", example = "서울 광진구, 서울 강남구, 서울 영등포구")
         String serviceAreas,
-        @Schema(description = "호출 가능 지역 코드", example = "[1, 2]")
-        List<Long> regionCodes,
+        @Schema(description = "호출 가능 지역 정보 리스트")
+        List<RegionResponse> regionCodes,
         @Schema(description = "푸드트럭 메뉴 카테고리 (라벨 리스트)", example = "[\"한식\",\"분식\"]")
         List<String> menuCategories,
         @Schema(description = "푸드트럭 제공 가능 수량", example = "200인분 미만")
@@ -49,6 +50,7 @@ public record FoodTruckDetailResponse(
 ) {
     public static FoodTruckDetailResponse from(FoodTruck foodTruck, List<FoodTruckServiceArea> serviceAreas, List<AvailableDate> availableDates, Boolean isSaved) {
         FoodTruckInfo foodTruckInfo = foodTruck.getFoodTruckInfo();
+        List<RegionResponse> regionResponseList = serviceAreas.stream().map(FoodTruckServiceArea::getRegion).map(region -> RegionResponse.of(region.getName(), region.getRegionId(), region.getRegionCode())).toList();
         return new FoodTruckDetailResponse(
                 foodTruck.getFoodTruckId(),
                 foodTruckInfo.getName(),
@@ -57,7 +59,7 @@ public record FoodTruckDetailResponse(
                 foodTruckInfo.getActiveTime(),
                 foodTruckInfo.getTimeDiscussRequired(),
                 foodTruck.getServiceAreas(serviceAreas),
-                serviceAreas.stream().map(FoodTruckServiceArea::getRegion).map(Region::getRegionCode).toList(),
+                regionResponseList,
                 foodTruckInfo.getMenuCategoryList().getMenuCategoryLabelList(),
                 foodTruckInfo.getAvailableQuantity().getValue(),
                 foodTruckInfo.getNeedElectricity().getValue(),
